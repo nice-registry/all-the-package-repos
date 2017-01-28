5219af45867728791bf5a8c990d2523b7679587d
Zeke Sikelianos <zeke@sikelianos.com>
2017-01-27 18:19:33 -0800

    user newer package-stream that does repo URL cleanup

--- scripts/build.js
+++ scripts/build.js
@@ -1,41 +1,18 @@
 const registry = require('package-stream')({
   db: 'https://replicate.npmjs.com'
 })
-const gh = require('github-url-to-object')
+const ora = require('ora')
+const spinner = ora('Loading').start()
 const repos = {}
 var totalPackages = 0
 
 registry
   .on('package', function (pkg) {
-    totalPackages++
-
-    process.stderr.write('.')
-    if (!pkg) return
-    if (!pkg.name) return
-    if (!pkg.repository) return
-
-    var repo
-    var gho
-
-    // normalize github url _strings_ to https format
-    if (typeof pkg.repository === 'string') {
-      gho = gh(pkg.repository)
-      repos[pkg.name] = gho ? gho.https_url : pkg.repository
-      return
-    }
-
-    // normalize github url _objects_ to https format
-    if (pkg.repository.url) {
-      gho = gh(pkg.repository.url)
-      repos[pkg.name] = gho ? gho.https_url : pkg.repository.url
-      return
-    }
+    spinner.text = String(++totalPackages)
+    if (!pkg || !pkg.name || !pkg.repository) return
+    repos[pkg.name] = pkg.repository
   })
   .on('up-to-date', function () {
-    const urls = Object.keys(repos).map(name => repos[name])
-    console.error(`\n${totalPackages} packages total`)
-    console.error(`${urls.length} packages with a repository`)
-    console.error(`${urls.filter(url => url.match('github.com')).length} packages with a github repository`)
     process.stdout.write(JSON.stringify(repos, null, 2))
     process.exit()
   })
