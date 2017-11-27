2d25aa0c2f843288f73ae3d8792e79ba518c0001
Zeke Sikelianos <zeke@sikelianos.com>
2017-11-26 20:52:05 -0800

    lint

--- hostnames.js
+++ hostnames.js
@@ -13,6 +13,6 @@ if (!module.parent) {
   const longestHostname = hostnames.sort((a, b) => b.length - a.length)[0]
 
   counts.forEach(hostname => {
-    console.log(pad(hostname.value, longestHostname.length+3) + String(hostname.count))
+    console.log(pad(hostname.value, longestHostname.length + 3) + String(hostname.count))
   })
 }
