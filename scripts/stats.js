4076959fbdd971ab5d4ccd9d8ea6a50039f55074
Zeke Sikelianos <zeke@sikelianos.com>
2018-12-24 14:40:21 -0800

    fix stats table to not rely on all-the-package-names

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,19 +1,17 @@
 const fs = require('fs')
 const path = require('path')
-const allNames = require('all-the-package-names').length
 const repos = Object.values(require('..'))
 const onGitHub = repos.filter(repo => repo.match('://github.com')).length
 const onBitBucket = repos.filter(repo => repo.match('://bitbucket.org')).length
 const onGitLab = repos.filter(repo => repo.match('://gitlab.com')).length
 
 const output = `<!-- stats -->
-Packages | Count | Percentage of Total Packages
--------- | ----- | ----------------------------
-Total in npm registry | ${allNames} | 100%
-With repository in package.json | ${repos.length} |  ${(repos.length / allNames * 100).toFixed(2)}%
-On GitHub | ${onGitHub} |  ${(onGitHub / allNames * 100).toFixed(2)}%
-On BitBucket | ${onBitBucket} |  ${(onBitBucket / allNames * 100).toFixed(2)}%
-On GitLab | ${onGitLab} |  ${(onGitLab / allNames * 100).toFixed(2)}%
+Packages | Count | Percentage
+-------- | ----- | ----------
+With repository in package.json | ${repos.length} | 100%
+On GitHub | ${onGitHub} |  ${(onGitHub / repos.length * 100).toFixed(2)}%
+On BitBucket | ${onBitBucket} |  ${(onBitBucket / repos.length * 100).toFixed(2)}%
+On GitLab | ${onGitLab} |  ${(onGitLab / repos.length * 100).toFixed(2)}%
 <!-- /stats -->`
 
 const readmeFile = path.join(__dirname, '../readme.md')
